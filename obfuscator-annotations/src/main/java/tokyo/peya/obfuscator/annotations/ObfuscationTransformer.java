64e40be2c141b5fbcb90ae1d424e9c51b5b3b734
PeyaPeyaPeyang <peyang@peya.tokyo>
2023-08-07 00:03:01 +0900

    chore: update copyright

--- obfuscator-annotations/src/main/java/tokyo/peya/obfuscator/annotations/ObfuscationTransformer.java
+++ obfuscator-annotations/src/main/java/tokyo/peya/obfuscator/annotations/ObfuscationTransformer.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2017-2019 superblaubeere27, Sam Sun, MarcoMC
+ * Copyright (c) 2023      Peyang 
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
@@ -16,7 +17,6 @@ public enum ObfuscationTransformer
     LINE_NUMBER_REMOVER,
     NUMBER_OBFUSCATION,
     STRING_ENCRYPTION,
-    HWID_PROTECTION,
     PEEPHOLE_OPTIMIZER,
     CRASHER,
     INVOKE_DYNAMIC,
