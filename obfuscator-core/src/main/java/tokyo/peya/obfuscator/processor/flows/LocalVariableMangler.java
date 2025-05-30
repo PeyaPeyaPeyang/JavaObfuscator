ffb3deb5c4abb76ad01f4b8b362f5e6f2cc7a6af
PeyaPeyaPeyang <peyang@peya.tokyo>
2025-05-30 22:29:43 +0900

    feat: Creating array for long/double

--- obfuscator-core/src/main/java/tokyo/peya/obfuscator/processor/flows/LocalVariableMangler.java
+++ obfuscator-core/src/main/java/tokyo/peya/obfuscator/processor/flows/LocalVariableMangler.java
@@ -19,6 +19,7 @@ import org.objectweb.asm.tree.IincInsnNode;
 import org.objectweb.asm.tree.InsnList;
 import org.objectweb.asm.tree.InsnNode;
 import org.objectweb.asm.tree.IntInsnNode;
+import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
 import org.objectweb.asm.tree.VarInsnNode;
 import org.objectweb.asm.tree.analysis.Analyzer;
@@ -39,6 +40,9 @@ class LocalVariableMangler
 {
     static void mangleLocalVariables(ProcessorCallback callback, ClassNode node, MethodNode method)
     {
+        InsnList instructions = method.instructions;
+
+        // 一時的にスタックサイズとローカル変数の最大値を変更し, ASM のフレーム解析を行う
         int maxStackSize = method.maxStack;
         int maxLocals = method.maxLocals;
         method.maxStack = 1337;
@@ -59,9 +63,6 @@ class LocalVariableMangler
 
         VariableProvider provider = new VariableProvider(method);
 
-        // Map of local variables and their types. They are added if the type of the variable is double, float, int or long
-        HashMap<Integer, Type> localVarMap = new HashMap<>();
-
         // KEY: Type of array
         // VALUE: Array local variable index
         HashMap<Type, Integer> typeArrayMap = new HashMap<>();
@@ -74,211 +75,225 @@ class LocalVariableMangler
         // VALUE: Current highest array index
         HashMap<Integer, Integer> arrayIndices = new HashMap<>();
 
-        //<editor-fold desc="Scanning">
+        // Map of local variables and their types.
+        // They are added if the type of the variable is double, float, int or long
+        HashMap<Integer, Type> localVarMap = scanLoads(instructions, provider, frames);
+
+        removeVoidInsn(localVarMap);
+
+        for (Map.Entry<Integer, Type> integerTypeEntry : localVarMap.entrySet())
+        {
+            if (!typeArrayMap.containsKey(integerTypeEntry.getValue()))
+                typeArrayMap.put(integerTypeEntry.getValue(), provider.allocateVar());
+
+            int index = typeArrayMap.get(integerTypeEntry.getValue());
+            int arrayIndex;
+
+            if (!arrayIndices.containsKey(index))
+                arrayIndices.put(index, 0);
+
+            arrayIndex = arrayIndices.get(index);
+
+            arrayIndices.put(index, arrayIndex + 1);
+            slotMap.put(integerTypeEntry.getKey(), new int[]{index, arrayIndex});
+        }
+
+        InsnList initialize = buildInstructions(typeArrayMap, arrayIndices);
         for (AbstractInsnNode abstractInsnNode : method.instructions.toArray())
         {
-            if (abstractInsnNode instanceof VarInsnNode)
+            if (abstractInsnNode instanceof VarInsnNode varInsnNode)
             {
-                VarInsnNode insnNode = (VarInsnNode) abstractInsnNode;
+                if (!slotMap.containsKey(varInsnNode.var))
+                    continue;
 
-                if (provider.isArgument(insnNode.var)) continue;
+                // Check if it is a load instruction
+                if (varInsnNode.getOpcode() == Opcodes.ILOAD
+                        || varInsnNode.getOpcode() == Opcodes.LLOAD
+                        || varInsnNode.getOpcode() == Opcodes.FLOAD
+                        || varInsnNode.getOpcode() == Opcodes.DLOAD)
+                    replaceLoadInstructions(instructions, varInsnNode, slotMap, localVarMap);
 
-                if (!localVarMap.containsKey(insnNode.var))
-                {
-                    Type t = null;
-
-                    switch (insnNode.getOpcode() - Opcodes.ILOAD)
-                    {
-                        case 0:
-                            t = Type.INT_TYPE;
-                            break;
-//                        case Opcodes.LLOAD - Opcodes.ILOAD:
-//                            t = Type.LONG_TYPE;
-//                            break;
-                        case Opcodes.FLOAD - Opcodes.ILOAD:
-                            t = Type.FLOAT_TYPE;
-                            break;
-//                        case Opcodes.DLOAD - Opcodes.ILOAD:
-//                            t = Type.DOUBLE_TYPE;
-//                            break;
-                    }
-                    if (t != null)
-                    {
-                        localVarMap.put(insnNode.var, t);
-                    }
-                }
-
-                if (insnNode.getOpcode() >= Opcodes.ISTORE && insnNode.getOpcode() <= Opcodes.ASTORE)
-                {
-                    Frame<SourceValue> currentFrame = frames[method.instructions.indexOf(insnNode)];
-
-                    SourceValue stack = currentFrame.getStack(currentFrame.getStackSize() - 1);
-
-                    if (stack.getSize() > 1)
-                    {
-                        localVarMap.put(insnNode.var, Type.VOID_TYPE);
-                    }
-                }
-            }
-        }
-        //</editor-fold>
-
-
-        {
-            List<Integer> remove = new ArrayList<>();
-
-            for (Map.Entry<Integer, Type> integerTypeEntry : localVarMap.entrySet())
-            {
-                if (integerTypeEntry.getValue().getSort() == Type.VOID)
-                {
-                    remove.add(integerTypeEntry.getKey());
-                }
+                // Check if it is a store instruction
+                if (abstractInsnNode.getOpcode() == Opcodes.ISTORE
+                        || abstractInsnNode.getOpcode() == Opcodes.LSTORE
+                        || abstractInsnNode.getOpcode() == Opcodes.FSTORE
+                        || abstractInsnNode.getOpcode() == Opcodes.DSTORE)
+                    replaceStoreInstructions(instructions, varInsnNode, slotMap, localVarMap);
             }
 
-            for (Integer integer : remove)
+            if (abstractInsnNode instanceof IincInsnNode iincInsnNode)
             {
-                localVarMap.remove(integer);
+                if (!slotMap.containsKey(iincInsnNode.var))
+                    continue;
+
+                replaceIncrements(instructions, iincInsnNode, slotMap, localVarMap);
             }
         }
 
-        {
-            for (Map.Entry<Integer, Type> integerTypeEntry : localVarMap.entrySet())
-            {
+        if (!localVarMap.isEmpty())
+            instructions.insertBefore(instructions.getFirst(), initialize);
 
-                if (!typeArrayMap.containsKey(integerTypeEntry.getValue()))
-                    typeArrayMap.put(integerTypeEntry.getValue(), provider.allocateVar());
+        callback.setForceComputeFrames();
+    }
 
-                int index = typeArrayMap.get(integerTypeEntry.getValue());
-                int arrayIndex;
+    private static void replaceIncrements(InsnList methodInstructions, IincInsnNode iincInsnNode, HashMap<Integer, int[]> slotMap,
+                                          HashMap<Integer, Type> localVarMap)
+    {
+        int[] value = slotMap.get(iincInsnNode.var);
 
-                if (!arrayIndices.containsKey(index))
-                {
-                    arrayIndices.put(index, 0);
-                }
+        InsnList replace = new InsnList();
 
-                arrayIndex = arrayIndices.get(index);
+        replace.add(new VarInsnNode(Opcodes.ALOAD, value[0]));
+        replace.add(NodeUtils.generateIntPush(value[1]));
 
-                arrayIndices.put(index, arrayIndex + 1);
+        replace.add(new VarInsnNode(Opcodes.ALOAD, value[0]));
+        replace.add(NodeUtils.generateIntPush(value[1]));
+        replace.add(new InsnNode(localVarMap.get(iincInsnNode.var).getOpcode(Opcodes.IALOAD)));
 
-                slotMap.put(integerTypeEntry.getKey(), new int[]{index, arrayIndex});
-            }
+        replace.add(NodeUtils.generateIntPush(iincInsnNode.incr));
+        replace.add(new InsnNode(Opcodes.IADD));
+
+        replace.add(new InsnNode(localVarMap.get(iincInsnNode.var).getOpcode(Opcodes.IASTORE)));
+
+        methodInstructions.insert(iincInsnNode, replace);
+        methodInstructions.remove(iincInsnNode);
+    }
+
+    private static void replaceStoreInstructions(InsnList methodInstructions, VarInsnNode varInsnNode,
+                                                 HashMap<Integer, int[]> slotMap, HashMap<Integer, Type> localVarMap) {
+
+        int[] value = slotMap.get(varInsnNode.var);
+        Type type = localVarMap.get(varInsnNode.var);
+
+        InsnList replace = new InsnList();
+
+        // long または double の値を正しく格納する: [value] -> [arrayREF, index, value]
+        if (type.getSize() == 2) {
+            // long/double（2 スタックスロット分を占有）の場合
+            replace.add(new InsnNode(Opcodes.DUP2)); // long/double の値を複製
+            replace.add(new VarInsnNode(Opcodes.ALOAD, value[0])); // 配列参照を読み込む
+            replace.add(NodeUtils.generateIntPush(value[1])); // インデックスをプッシュ
+            replace.add(new InsnNode(Opcodes.DUP2_X2)); // スタックの順序を調整
+            replace.add(new InsnNode(Opcodes.POP2));    // 不要な値を除去
+        } else {
+            // int/float の場合
+            replace.add(new VarInsnNode(Opcodes.ALOAD, value[0])); // 配列参照を読み込む
+            replace.add(NodeUtils.generateIntPush(value[1]));      // インデックスをプッシュ
+            replace.add(new InsnNode(Opcodes.SWAP));               // 値とインデックスを入れ替える
         }
 
+        replace.add(new InsnNode(type.getOpcode(Opcodes.IASTORE))); // Store
+
+        methodInstructions.insert(varInsnNode, replace);
+        methodInstructions.remove(varInsnNode);
+    }
+
+    private static void replaceLoadInstructions(InsnList methodInstructions, VarInsnNode varInsnNode,
+                                  HashMap<Integer, int[]> slotMap, HashMap<Integer, Type> localVarMap)
+    {
+        int[] value = slotMap.get(varInsnNode.var);
+
+        InsnList replace = new InsnList();
+        replace.add(new VarInsnNode(Opcodes.ALOAD, value[0]));
+        replace.add(NodeUtils.generateIntPush(value[1]));
+        replace.add(new InsnNode(localVarMap.get(varInsnNode.var).getOpcode(Opcodes.IALOAD)));
+
+        methodInstructions.insert(varInsnNode, replace);
+        methodInstructions.remove(varInsnNode);
+    }
+
+    private static InsnList buildInstructions(HashMap<Type, Integer> typeArrays, HashMap<Integer, Integer> arrayIndices)
+    {
         InsnList initialize = new InsnList();
-
-        for (Map.Entry<Type, Integer> integerTypeEntry : typeArrayMap.entrySet())
+        for (Map.Entry<Type, Integer> integerTypeEntry : typeArrays.entrySet())
         {
             int arrayType = integerTypeEntry.getKey().getSort();
 
-            switch (arrayType)
+            arrayType = switch (arrayType)
             {
-                case Type.INT:
-                    arrayType = Opcodes.T_INT;
-                    break;
-                case Type.LONG:
-                    arrayType = Opcodes.T_LONG;
-                    break;
-                case Type.DOUBLE:
-                    arrayType = Opcodes.T_DOUBLE;
-                    break;
-                case Type.FLOAT:
-                    arrayType = Opcodes.T_FLOAT;
-                    break;
-                default:
-                    throw new IllegalArgumentException();
-            }
+                case Type.INT -> Opcodes.T_INT;
+                case Type.LONG -> Opcodes.T_LONG;
+                case Type.DOUBLE -> Opcodes.T_DOUBLE;
+                case Type.FLOAT -> Opcodes.T_FLOAT;
+                default -> throw new IllegalArgumentException();
+            };
 
             initialize.add(NodeUtils.generateIntPush(arrayIndices.get(integerTypeEntry.getValue())));
             initialize.add(new IntInsnNode(Opcodes.NEWARRAY, arrayType));
             initialize.add(new VarInsnNode(Opcodes.ASTORE, integerTypeEntry.getValue()));
         }
 
-        for (AbstractInsnNode abstractInsnNode : method.instructions.toArray())
+        return initialize;
+    }
+
+    private static void removeVoidInsn(HashMap<Integer, Type> localVarMap)
+    {
+        List<Integer> remove = new ArrayList<>();
+
+        for (Map.Entry<Integer, Type> integerTypeEntry : localVarMap.entrySet())
+            if (integerTypeEntry.getValue().getSort() == Type.VOID)
+                remove.add(integerTypeEntry.getKey());
+
+        for (Integer integer : remove)
+            localVarMap.remove(integer);
+    }
+
+    private static HashMap<Integer, Type> scanLoads(InsnList methodInstructions, VariableProvider provider,
+                                  Frame<SourceValue>[] frames)
+    {
+        HashMap<Integer, Type> localVarMapToStore = new HashMap<>();
+
+        for (AbstractInsnNode abstractInsnNode : methodInstructions.toArray())
         {
-            if (abstractInsnNode instanceof VarInsnNode)
+            if (!(abstractInsnNode instanceof VarInsnNode insnNode))
+                continue;
+
+            if (provider.isArgument(insnNode.var))
+                continue;
+
+            if (!localVarMapToStore.containsKey(insnNode.var))
             {
-                VarInsnNode varInsnNode = (VarInsnNode) abstractInsnNode;
-
-                if (slotMap.containsKey(varInsnNode.var))
+                Type t = switch (insnNode.getOpcode())  // ロード系
                 {
-                    // Check if it is a load instruction
-                    if (abstractInsnNode.getOpcode() == Opcodes.ILOAD
-                            || abstractInsnNode.getOpcode() == Opcodes.LLOAD
-                            || abstractInsnNode.getOpcode() == Opcodes.FLOAD
-                            || abstractInsnNode.getOpcode() == Opcodes.DLOAD)
-                    {
-                        int[] value = slotMap.get(varInsnNode.var);
+                    case Opcodes.ILOAD -> Type.INT_TYPE;
+                    case Opcodes.LLOAD -> Type.LONG_TYPE;
+                    case Opcodes.FLOAD -> Type.FLOAT_TYPE;
+                    case Opcodes.DLOAD -> Type.DOUBLE_TYPE;
+                    default -> null;
+                };
 
-                        InsnList replace = new InsnList();
-
-                        replace.add(new VarInsnNode(Opcodes.ALOAD, value[0]));
-                        replace.add(NodeUtils.generateIntPush(value[1]));
-                        replace.add(new InsnNode(localVarMap.get(varInsnNode.var).getOpcode(Opcodes.IALOAD)));
-
-                        method.instructions.insert(varInsnNode, replace);
-                        method.instructions.remove(varInsnNode);
-                    }
-                    // Check if it is a store instruction
-                    if (abstractInsnNode.getOpcode() == Opcodes.ISTORE
-                            || abstractInsnNode.getOpcode() == Opcodes.LSTORE
-                            || abstractInsnNode.getOpcode() == Opcodes.FSTORE
-                            || abstractInsnNode.getOpcode() == Opcodes.DSTORE)
-                    {
-                        int[] value = slotMap.get(varInsnNode.var);
-
-
-                        InsnList replace = new InsnList();
-
-
-                        Type type = localVarMap.get(varInsnNode.var);
-
-
-                        replace.add(new VarInsnNode(Opcodes.ALOAD, value[0]));
-                        replace.add(new InsnNode(Opcodes.SWAP));
-                        replace.add(NodeUtils.generateIntPush(value[1]));
-                        replace.add(new InsnNode(Opcodes.SWAP));
-                        replace.add(new InsnNode(type.getOpcode(Opcodes.IASTORE)));
-
-                        method.instructions.insert(varInsnNode, replace);
-                        method.instructions.remove(varInsnNode);
-                    }
-                }
+                if (t != null)
+                    localVarMapToStore.put(insnNode.var, t);
             }
 
-            if (abstractInsnNode instanceof IincInsnNode)
+            if (insnNode.getOpcode() >= Opcodes.ISTORE && insnNode.getOpcode() <= Opcodes.ASTORE)
             {
-                IincInsnNode iincInsnNode = (IincInsnNode) abstractInsnNode;
+                Frame<SourceValue> currentFrame = frames[methodInstructions.indexOf(insnNode)];
+                SourceValue stack = currentFrame.getStack(currentFrame.getStackSize() - 1);
 
-                if (slotMap.containsKey(iincInsnNode.var))
+                if (stack.getSize() > 1) // LONG と DOUBLE の場合
                 {
-                    int[] value = slotMap.get(iincInsnNode.var);
-
-                    InsnList replace = new InsnList();
-
-                    replace.add(new VarInsnNode(Opcodes.ALOAD, value[0]));
-                    replace.add(NodeUtils.generateIntPush(value[1]));
-
-                    replace.add(new VarInsnNode(Opcodes.ALOAD, value[0]));
-                    replace.add(NodeUtils.generateIntPush(value[1]));
-                    replace.add(new InsnNode(localVarMap.get(iincInsnNode.var).getOpcode(Opcodes.IALOAD)));
-
-                    replace.add(NodeUtils.generateIntPush(iincInsnNode.incr));
-
-                    replace.add(new InsnNode(Opcodes.IADD));
-
-                    replace.add(new InsnNode(localVarMap.get(iincInsnNode.var).getOpcode(Opcodes.IASTORE)));
-
-                    method.instructions.insert(iincInsnNode, replace);
-                    method.instructions.remove(iincInsnNode);
+                    Type type = guessTypeFromFrame(stack);
+                    if (type != null && !localVarMapToStore.containsKey(insnNode.var))
+                        localVarMapToStore.put(insnNode.var, type);
                 }
             }
-
         }
 
+        return localVarMapToStore;
+    }
+    private static Type guessTypeFromFrame(SourceValue value)
+    {
+        for (AbstractInsnNode insn : value.insns)
+            switch (insn.getOpcode()) {
+                case Opcodes.LSTORE, Opcodes.LLOAD, Opcodes.LADD, Opcodes.LSUB -> {
+                    return Type.LONG_TYPE;
+                }
+                case Opcodes.DSTORE, Opcodes.DLOAD, Opcodes.DADD, Opcodes.DSUB -> {
+                    return Type.DOUBLE_TYPE;
+                }
+            }
 
-        if (localVarMap.size() > 0)
-            method.instructions.insertBefore(method.instructions.getFirst(), initialize);
-
-        callback.setForceComputeFrames();
+        return null;
     }
 }
